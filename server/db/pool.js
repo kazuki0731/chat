9cc7f747083f44d7525208d092a2fb142b1eb532
kawabata <kazuki.kawabata02@gmail.com>
2021-09-26 14:44:37 +0900

    init

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,12 +1,24 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  database: "newtodo",
-  user: "kawabata",
-  host: "localhost",
-  password: "XLwdAX2m",
+  database: process.env.ENV_DB,
+  user: process.env.ENV_USER,
+  password: process.env.ENV_PASSWORD,
+  host: process.env.ENV_HOST,
   port: 5432,
+  ssl: {
+    sslmode: "require",
+    rejectUnauthorized: false,
+  },
 });
 
+// const pool = new Pool({
+//   database: "newtodo",
+//   user: "kawabata",
+//   host: "localhost",
+//   password: "XLwdAX2m",
+//   port: 5432,
+// });
 
 module.exports = pool;
